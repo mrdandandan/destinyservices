df78ce56b17cc3a87309f2704892574636277fce
Dan <mrdandandan@dannymiller.me>
2018-08-27 18:23:50 -0400

    D2 (#3)
    * Update things for d2
    
    * Cors
    
    * Try and cut out outliers

--- src/services/matchmaking/generateTeams.js
+++ src/services/matchmaking/generateTeams.js
@@ -6,7 +6,7 @@ function generateTeams(players = [], aggregate = 'killsDeathsRatio') {
     }, 0) / arr.length;
 
     let iterations = 0,
-        maxIterations = 50,
+        maxIterations = Math.pow(players.length, 2) * 2,
         possibleTeams = [],
         allAggregates = Object.keys(players[0].aggregateStats);
 
@@ -59,6 +59,13 @@ function generateTeams(players = [], aggregate = 'killsDeathsRatio') {
         return 0;
     });
 
+    let maxPossibleTeams = 6;
+    if(players.length <= 4) {
+        maxPossibleTeams = 2;
+    } else if(players.length <= 6) {
+        maxPossibleTeams = 4;
+    }
+
     let limitedTeams = possibleTeams.slice(0, possibleTeams.length > 5 ? 5 : possibleTeams.length),
         teams = limitedTeams[randomIndex(limitedTeams)],
         output = {
@@ -68,7 +75,8 @@ function generateTeams(players = [], aggregate = 'killsDeathsRatio') {
             aggregateAverageDifference: teams.aggregateAverageDifference,
             aggregateStats: {},
             alpha: [],
-            bravo: []
+            bravo: [],
+            maxPossibleTeams
         };
 
     teams.alpha.forEach(player => {
@@ -101,102 +109,3 @@ function isDuplicateArray(arr1, arr2) {
 }
 
 module.exports = generateTeams;
-
-// const players = [
-//     {
-//         "displayName": "Sv rocks 3",
-//         "aggregateStats": {
-//             "assists": 10316,
-//             "combatRating": 105.19,
-//             "deaths": 31028,
-//             "kills": 44959,
-//             "killsDeathsAssists": 1.64,
-//             "killsDeathsRatio": 1.48,
-//             "winLossRatio": 1.69
-//         }
-//     },
-//     {
-//         "displayName": "XXL JIGSAW",
-//         "aggregateStats": {
-//             "assists": 22754,
-//             "combatRating": 96.06,
-//             "deaths": 58115,
-//             "kills": 81092,
-//             "killsDeathsAssists": 1.58,
-//             "killsDeathsRatio": 1.38,
-//             "winLossRatio": 2.07
-//         }
-//     },
-//     {
-//         "displayName": "mistervischous",
-//         "aggregateStats": {
-//             "assists": 38777,
-//             "combatRating": 86.29,
-//             "deaths": 99173,
-//             "kills": 136726,
-//             "killsDeathsAssists": 1.54,
-//             "killsDeathsRatio": 1.34,
-//             "winLossRatio": 1.22
-//         }
-//     },
-//     {
-//         "displayName": "A tasty samichx",
-//         "aggregateStats": {
-//             "assists": 7572,
-//             "combatRating": 95.55,
-//             "deaths": 23679,
-//             "kills": 29646,
-//             "killsDeathsAssists": 1.49,
-//             "killsDeathsRatio": 1.34,
-//             "winLossRatio": 1.44
-//         }
-//     },
-//     {
-//         "displayName": "Slothee",
-//         "aggregateStats": {
-//             "assists": 15291,
-//             "combatRating": 71.02,
-//             "deaths": 30663,
-//             "kills": 37421,
-//             "killsDeathsAssists": 1.49,
-//             "killsDeathsRatio": 1.23,
-//             "winLossRatio": 0.86
-//         }
-//     },
-//     {
-//         "displayName": "Riv3rmon5ters",
-//         "aggregateStats": {
-//             "assists": 8554,
-//             "combatRating": 94.02,
-//             "deaths": 21707,
-//             "kills": 25037,
-//             "killsDeathsAssists": 1.24,
-//             "killsDeathsRatio": 1.06,
-//             "winLossRatio": 1.13
-//         }
-//     },
-//     {
-//         "displayName": "Thorecw00tstick",
-//         "aggregateStats": {
-//             "assists": 15684,
-//             "combatRating": 74.48,
-//             "deaths": 44216,
-//             "kills": 49020,
-//             "killsDeathsAssists": 1.23,
-//             "killsDeathsRatio": 1.06,
-//             "winLossRatio": 1.37
-//         }
-//     },
-//     {
-//         "displayName": "DJaye",
-//         "aggregateStats": {
-//             "assists": 20966,
-//             "combatRating": 68.57,
-//             "deaths": 56223,
-//             "kills": 55270,
-//             "killsDeathsAssists": 1.18,
-//             "killsDeathsRatio": 0.99,
-//             "winLossRatio": 1.68
-//         }
-//     }
-// ];
